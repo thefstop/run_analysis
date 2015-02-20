ef5750c011728656d207be4776a5809a95bf833a
thefstop <jmc327@gmail.com>
2015-02-19 18:47:25 -0600

    Updated run_analysis.r

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,11 @@
 run_analysis <- function() {
-  
-  
-  ##Load the data required for analysis
-  
+    
   library(dplyr)
   ##Acts are the possible activities. Used as labels for factors
   acts<- c("Walking", "Walking_Upstairs", "Walking_Downstairs", "Sitting", "Standing", "Laying")
   
-  #Load test data. y_test is loaded as a vector so that it can be
-  #conerted to a factor, using the 'acts' variables as label names
+  #Load test data. y_test and y_train are loaded as a vector so that it can be
+  #coneverted to a factor, using the 'acts' variables as label names
   x_test <- read.table("Dataset/test/X_test.txt") 
   y_test<-as.numeric(scan("Dataset/test/y_test.txt", sep="\n"))
   subject_test<-read.table("Dataset/test/subject_test.txt")
@@ -25,15 +22,15 @@ run_analysis <- function() {
   test_db<-bind_cols(subject_test, acts_test, x_test)
   train_db<-bind_cols(subject_train, acts_train, x_train)
  
-    #Load the identifying data for the features to use as column names
+  #Load the identifying data for the features to use as column names
   
   features <-read.table("Dataset/features.txt", sep=" ")
-  features<-as.vector(features[,2])
+  features <-as.vector(features[,2])
   
   #Add descriptive column names, two I've typed in and the rest from the
-  #features.txt. The as.vector step strips out the numbers that are a part of features.txt
-  colnames(test_db)<-c("Subject_ID", "Activity", features)
-  colnames(train_db)<-c("Subject_ID", "Activity", features)
+  #features.txt. 
+  colnames(test_db)  <-c("Subject_ID", "Activity", features)
+  colnames(train_db) <-c("Subject_ID", "Activity", features)
   
   #Combine the test and training databases
   master_db<- bind_rows(test_db, train_db)
@@ -41,13 +38,18 @@ run_analysis <- function() {
   #Select only the columns specified,and those that contain mean() or std()
   trim_db<-select(master_db, Subject_ID, Activity, contains("mean()"), contains("std()"))
   
-  View(trim_db)
-  #Create the separate database, in 'wide view' that contains the avergae
+  #View(trim_db) 
+  #Uncomment previous line for testing 
+  
+  #Create the separate database, in 'wide view' that contains the average
   #for each variable. First use the group_by command to group by subject and
   #activity. Then use sumarise_each to apply the mean to each variable.
   avg_db <- group_by(trim_db, Subject_ID, Activity)
   result_db<-summarise_each(avg_db, funs(mean))
-  View(result_db)
+  
+  #View(result_db)
+  #Uncoment previous line for testing
+  
   #Output the tidy results for submission 
   write.table(result_db, file="tidydb.txt", row.names= FALSE)
 }
